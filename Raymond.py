11119ee59eea964586948d163939d8dd7fa91374
tanmaysachan <tnmysachan@gmail.com>
2021-04-12 09:25:52 +0530

    add raymond (not working)

---
+++ Raymond.py
@@ -0,0 +1,109 @@
+import time
+from time import sleep
+from mpi4py import MPI
+import numpy as np
+import random
+from queue import Queue
+from heapq import *
+
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+rank = comm.Get_rank()
+
+# Defines the local time for this process
+local_time = 0
+
+def update_local_time(new_timestamp):
+    global local_time
+    local_time = max(local_time + 1, new_timestamp + 1)
+
+class Raymond:
+    def __init__(self):
+        self.holder = max(0, (rank-1)//2)
+        self.request_q = Queue()
+        self.asked = False
+
+    def loop_node(self):
+        req = comm.irecv()
+        while True:
+            if not self.asked and random.random() < 0.2:
+                print('requesting, ', rank, flush=True)
+                self.make_request(1)
+
+            # Try receiving a message
+            data = req.test()
+            if data[0] == True:
+                req = comm.irecv()
+
+            if not self.request_q.empty() and self.request_q.queue[0] == rank \
+                        and self.asked == True and self.holder == rank:
+                # pop before exec
+                self.request_q.get()
+
+                self.asked = False
+                self.exec_cs()
+
+            if self.holder == rank and self.asked == False \
+                    and not self.request_q.empty():
+                self.transfer_privilege()
+
+            # Request Message
+            if data[0] == True and data[1] is not None and data[1][1] == 0:
+                sender = data[1][0]
+                # print('request reached ', rank, ' from ', sender, flush=True)
+                self.request_q.put(sender)
+                self.make_request(0)
+
+            # Receive Privilege
+            if data[0] == True and data[1] is not None and data[1][1] == 1:
+                sender = data[1][0]
+                self.holder = rank
+
+            print('TT ', rank, self.request_q.queue, flush=True)
+            if self.holder == rank:
+                print('privilege at ', rank, flush=True)
+            sleep(random.random())
+
+    def make_request(self, t):
+        if self.holder != rank:
+            # request originates
+            if t == 1:
+                self.asked = True
+                self.request_q.put(rank)
+            msg = (rank, 0)
+            # print('msg sent from ', rank, ' to ', self.holder, flush=True)
+            comm.send(msg, dest=self.holder)
+        elif t == 1:
+            self.asked = True
+            self.request_q.put(rank)
+
+    def transfer_privilege(self):
+        rec = self.request_q.get()
+        print('transferring from ', rank, ' to ', rec, flush=True)
+        self.holder = rec
+        msg = (rank, 1)
+        comm.send(msg, dest=rec)
+
+    def exec_cs(self):
+        print('executing cs for', rank, '...', flush=True)
+        update_local_time(local_time)
+        critical_function()
+
+
+def critical_function(filename1='tmp.txt'):
+    with open(filename1, 'w+') as f:
+        f.write(str(rank))
+
+    sleep(2)
+    with open(filename1, 'r') as f:
+        line = f.readline().strip()
+        if line == '' or int(line) != rank:
+            print("\n\n\n******CS FAILED*****\nExpected {} found {}\n\n".format(rank, int(line)), flush=True)
+            
+        else:
+            print("CS PASSED", flush=True)
+
+
+if __name__ == '__main__':
+    algo = Raymond()
+    algo.loop_node()
